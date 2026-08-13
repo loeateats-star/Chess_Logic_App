d163f60e72d328d3e5a615f311ebc060d2d4de07
Lohit Polisettie <loeateats@gmail.com>
2026-08-13 18:04:32 -0500

    Prototype

--- app.py
+++ app.py
@@ -194,6 +194,21 @@ def curriculum_section(section_slug):
     )
 
 
+@app.route('/learn')
+def learn():
+    return render_template('learn.html')
+
+
+@app.route('/join')
+def join():
+    return render_template('join.html')
+
+
+@app.route('/donate')
+def donate():
+    return render_template('donate.html')
+
+
 @app.route('/basics')
 def basics():
     return render_template('basics.html')
